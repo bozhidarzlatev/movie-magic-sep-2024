0f3412cc09fe39a4bffb35cbb3e45ea3c5602356
bozhidarzlatev <b.zlatev@outlook.com>
2024-09-25 14:31:19 +0300

    Setup express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log(`Nice to see you!`);
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(`Hello`)
+})
+
+app.listen(5000, () => console.log('Server is listening on http://localhost:5000'))
